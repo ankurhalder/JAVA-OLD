1d3a23c58a8209f77d21139a2f05c149dbdd3538
Ankur Halder <115826489+ankurhalder@users.noreply.github.com>
2024-02-21 12:46:05 +0530

    Refactor print statement in Main.java

--- mylearning/Main.java
+++ mylearning/Main.java
@@ -2,7 +2,7 @@ package mylearning;
 public class Main {
     public static void main (String[] args){
         System.out.println("Hello World!");
-        System.out.println("Hello World!");
+        System.out.print("Hello World!\n");
         System.out.print("Hello World!");
 
     }
