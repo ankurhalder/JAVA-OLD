51501b8b020debe2896b15fe76046e4cd9cff4d5
Ankur Halder <115826489+ankurhalder@users.noreply.github.com>
2024-02-21 12:45:38 +0530

    Add print statement to display "Hello World!"

--- mylearning/Main.java
+++ mylearning/Main.java
@@ -2,6 +2,7 @@ package mylearning;
 public class Main {
     public static void main (String[] args){
         System.out.println("Hello World!");
+        System.out.println("Hello World!");
         System.out.print("Hello World!");
 
     }
