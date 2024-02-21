264a8a4ec45c01ca5c5176710ab3b22ffa9bb5e3
Ankur Halder <115826489+ankurhalder@users.noreply.github.com>
2024-02-21 14:25:39 +0530

    Refactor input handling and add addition functionality

--- input/Input.java
+++ input/Input.java
@@ -4,11 +4,18 @@ import java.util.*;
 public class Input {
     public static void main(String[] args) {
         // Input
-        Scanner sc = new Scanner(System.in);
-        String name = sc.nextLine();
+        // Scanner sc = new Scanner(System.in);
+        // String name = sc.nextLine();
         // nextInt()
         // nextDouble() and more
-        System.out.println(name);
-        sc.close(); 
+        // System.out.println(name);
+        // sc.close(); 
+
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = a + b;
+        System.out.println(c);
+        sc.close();
     }
 }
