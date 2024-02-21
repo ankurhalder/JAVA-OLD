273046dc6eee1b54ba869ab2f2a1c99d19fadf86
Ankur Halder <115826489+ankurhalder@users.noreply.github.com>
2024-02-21 14:01:38 +0530

    Refactor variable names for clarity

---
+++ variables/Variables.java
@@ -0,0 +1,12 @@
+package variables;
+
+public class Variables {
+    @SuppressWarnings("unused")
+    public static void main(String[] args){
+        String name = "Ankur Halder";
+        int a = 25 ;
+        int  b = 10;
+        double price = 25.25;
+         b = 20;
+    }
+}
