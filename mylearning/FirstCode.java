c23afb630dee74d78ed15c89465bc54aa98811f3
Ankur Halder <115826489+ankurhalder@users.noreply.github.com>
2024-02-21 12:45:19 +0530

    Remove FirstCode.java and Main.java

--- mylearning/FirstCode.java
+++ mylearning/FirstCode.java
@@ -1,3 +1,4 @@
+package mylearning;
 class FirstCode{
   public static void main(String args[]){
     System.out.print("Hello World");
--- mylearning/Main.java
+++ mylearning/Main.java
@@ -1,3 +1,4 @@
+package mylearning;
 public class Main {
     public static void main (String[] args){
         System.out.println("Hello World!");
