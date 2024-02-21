eae0ac5eb7b21cfff7e757d7a10f10ad81b25b0a
Ankur Halder <115826489+ankurhalder@users.noreply.github.com>
2024-02-21 16:06:18 +0530

    1 to 10 counter done in do-while loop

---
+++ loops/DowhileCounter.java
@@ -0,0 +1,20 @@
+package loops;
+
+import java.util.*;
+
+public class DowhileCounter {
+   public static void main (String args[]){
+
+    Scanner sc = new Scanner(System.in);
+     int counter = sc.nextInt();
+     
+     int i = 0; 
+     do{
+       System.out.println(i);
+       i = i +1;
+     }
+     while(i <= counter);
+
+    sc.close();
+   }
+}
