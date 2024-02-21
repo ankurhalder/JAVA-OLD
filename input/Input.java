fd12ff72d448b558cdcf1fc79082e17c1a2b27db
Ankur Halder <115826489+ankurhalder@users.noreply.github.com>
2024-02-21 14:23:17 +0530

    Refactor code to improve performance and readability

---
+++ input/Input.java
@@ -0,0 +1,14 @@
+package input;
+import java.util.*;
+
+public class Input {
+    public static void main(String[] args) {
+        // Input
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        // nextInt()
+        // nextDouble() and more
+        System.out.println(name);
+        sc.close(); 
+    }
+}
