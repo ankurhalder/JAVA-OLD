8b2a435cb0403d22792e29dd017d37d57f04ec82
Ankur Halder <115826489+ankurhalder@users.noreply.github.com>
2024-02-22 11:22:55 +0530

    butterfly learned

---
+++ advance_patterns/Butterfly.java
@@ -0,0 +1,45 @@
+package advance_patterns;
+
+import java.util.*;
+public class Butterfly {
+   public static void main(String args[]){
+    
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    sc.close();
+    // First Upper Half
+    for(int i = 1 ; i <= n; i++){
+        // First part
+        for(int j = 1 ; j <= i; j++){
+         System.out.print(" "+"*");
+        } 
+        int spaces = 2* (n-i);
+       // Spaces
+        for (int j = 1 ; j <=spaces; j++){
+        System.out.print(" "+" ");
+        }
+      // 2nd part
+       for(int j=1; j<=i; j++){
+        System.out.print(" "+"*");
+       }
+       System.out.println();
+    }
+    // Lower half
+    for(int i = n ; i >= 1; i--){
+        // First part
+        for(int j = 1 ; j <= i; j++){
+         System.out.print(" "+"*");
+        } 
+        int spaces = 2* (n-i);
+       // Spaces
+        for (int j = 1 ; j <=spaces; j++){
+        System.out.print(" "+" ");
+        }
+      // 2nd part
+       for(int j=1; j<=i; j++){
+        System.out.print(" "+"*");
+       }
+       System.out.println();
+    }
+   } 
+}
