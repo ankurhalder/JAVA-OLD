15c9837cc398564fa30696ab31879fb9c8952333
Ankur Halder <115826489+ankurhalder@users.noreply.github.com>
2024-02-22 14:06:54 +0530

    function which takes in 2 numbers and returns the greater of those two

---
+++ ExercisePartOne/GreaterNumber.java
@@ -0,0 +1,21 @@
+package ExercisePartOne;
+
+import java.util.Scanner;
+
+public class GreaterNumber {
+
+        public static int greaterNumber(int a , int b){
+            if(a > b)
+             return a;
+            else
+            return b;
+        }
+        public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int greaterNumber = greaterNumber(a, b);
+        System.out.println("Between " + a + " and " + b + " Greater Number is " + greaterNumber);
+        sc.close();
+        }
+}
