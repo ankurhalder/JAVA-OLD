f65751655cc6279c3721c4503bf787354e4189ca
Ankur Halder <115826489+ankurhalder@users.noreply.github.com>
2024-02-22 16:21:09 +0530

    Refactor code to improve performance and readability

---
+++ arrays/MinMax.java
@@ -0,0 +1,32 @@
+package arrays;
+
+import java.util.Scanner;
+
+public class MinMax {
+    public static void main(String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the Number of integers you wan to add in the array \n");
+        int number = sc.nextInt();
+        int numbers[] = new int[number];
+
+        for (int i = 0; i < number; i++) {
+            System.out.print("Enter " + i + " index number \n");
+            numbers[i] = sc.nextInt();
+        }
+        int max = numbers[0];
+        int min = numbers[0];
+        for (int i = 0; i < numbers.length; i++) {
+            if (max > numbers[i]) {
+                max = numbers[i];
+            } else {
+                min = numbers[i];
+            }
+
+        }
+
+        System.out.println("The Maximum number in the Array is " + max);
+        System.out.println("The Minimum number in the Array is " + min);
+        sc.close();
+    }
+}
