bb7bdb354822fb3a4c9ee41fb5624bb2b7c54fe1
Ankur Halder <115826489+ankurhalder@users.noreply.github.com>
2024-02-22 13:47:02 +0530

    boilerPlate Created

---
+++ ExercisePartOne/SumOfOdd.java
@@ -0,0 +1,14 @@
+package ExercisePartOne;
+
+import java.util.Scanner;
+
+public class SumOfOdd {
+
+    public static void main(String args[]){
+
+      Scanner sc = new Scanner(System.in);
+
+   
+      sc.close();
+    }
+}
