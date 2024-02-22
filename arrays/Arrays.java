58469b20754fd05c34ede4ce3047ca8df5b464cd
Ankur Halder <115826489+ankurhalder@users.noreply.github.com>
2024-02-22 15:46:21 +0530

    Refactor array declaration in Arrays.java

--- arrays/Arrays.java
+++ arrays/Arrays.java
@@ -5,15 +5,14 @@ import java.util.Scanner;
 public class Arrays {
     public static void main(String args[]) {
         // int[] marks = new int[10];
+        // int marks[] = new int[10];
         // marks[0] = 97;
         // marks[1] = 98;
         // marks[2] = 99;
         // System.out.println(marks[0]);
         // System.out.println(marks[1]);
         // System.out.println(marks[2]);
-
         Scanner sc = new Scanner(System.in);
-
         sc.close();
     }
 }
