0d8965f49fe32e49abcd37c5e67f0c84fd1f6a49
Ankur Halder <115826489+ankurhalder@users.noreply.github.com>
2024-02-23 18:24:09 +0530

    Refactor input and generateArray functions

--- BoilerplateArr.java
+++ BoilerplateArr.java
@@ -2,24 +2,24 @@ import java.util.*;
 
 public class BoilerplateArr {
     public static void main(String[] args) {
-        System.out.println("Please mention the size of the array ");
+        System.out.println("Please enter the size of the array: ");
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
         int[] arr = input(n);
 
-        // Perform all the operation's here
+        // Perform all the operations here with the inputted array
         generateArray(arr);
         sc.close();
     }
 
+    // This function takes the size of the array as input and returns the array
     public static int[] input(int n) {
         Scanner sc = new Scanner(System.in);
-        int arr[] = new int[n];
+        int[] arr = new int[n];
 
         for (int i = 0; i < n; i++) {
             System.out.print("Enter element at index " + i + ": ");
-
             arr[i] = sc.nextInt();
         }
 
@@ -28,12 +28,11 @@ public class BoilerplateArr {
         return arr;
     }
 
+    // This function takes an array as input and generates the array
     public static void generateArray(int[] arr) {
-
         System.out.println("Generated array:");
 
         for (int i = 0; i < arr.length; i++) {
-
             System.out.print(arr[i] + " ");
         }
     }
