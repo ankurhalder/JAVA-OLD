54d28ab2fce02d3e84208d8e402e88f0be4247f2
Ankur Halder <115826489+ankurhalder@users.noreply.github.com>
2024-02-23 18:05:02 +0530

    Refactor Bubble.java to include user input and array generation

--- Sorting_Methods/Bubble.java
+++ Sorting_Methods/Bubble.java
@@ -1,13 +1,40 @@
 package Sorting_Methods;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class Bubble {
-    public static void main(String args[]) {
-
+    public static void main(String[] args) {
+        System.out.println("Please mention the size of the array ");
         Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = input(n);
 
-        System.out.println();
+        generateArray(arr);
         sc.close();
+    }
+
+    public static int[] input(int n) {
+        Scanner sc = new Scanner(System.in);
+        int arr[] = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("Enter element at index " + i + ": ");
+
+            arr[i] = sc.nextInt();
+        }
+
+        sc.close();
+
+        return arr;
+    }
+
+    public static void generateArray(int[] arr) {
+
+        System.out.println("Generated array:");
+
+        for (int i = 0; i < arr.length; i++) {
+
+            System.out.println(arr[i]);
+        }
     }
 }
