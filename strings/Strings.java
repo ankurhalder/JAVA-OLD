d08578cc08d057c73204a811362adad55a00a41b
Ankur Halder <115826489+ankurhalder@users.noreply.github.com>
2024-02-23 14:04:57 +0530

    Add comments and update StringBuilder in Strings.java

--- strings/Strings.java
+++ strings/Strings.java
@@ -4,14 +4,17 @@ import java.util.*;
 
 public class Strings {
     public static void main(String args[]) {
+        // Initiate
         StringBuilder sb = new StringBuilder("Ankur Halder");
         Scanner sc = new Scanner(System.in);
-
+        // print
         System.out.println(sb.charAt(0));
 
+        // update
         sb.setCharAt(0, 'M');
         System.out.println(sb);
 
+        // add
         sb.insert(1, "o");
         System.out.println(sb);
 
