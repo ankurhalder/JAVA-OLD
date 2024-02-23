0f80a017734617c3cf26f9f002efc995bb02a2bb
Ankur Halder <115826489+ankurhalder@users.noreply.github.com>
2024-02-23 14:00:29 +0530

    String builder

---
+++ strings/Strings.java
@@ -0,0 +1,14 @@
+package strings;
+
+import java.util.*;
+
+public class Strings {
+    public static void main(String args[]) {
+        StringBuilder sb = new StringBuilder("Ankur Halder");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println(sb.charAt(0));
+
+        sc.close();
+    }
+}
